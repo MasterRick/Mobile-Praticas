d969e00c24034457d5821fa7ec22ffebc6285525
rsls <16372495+MasterRick@users.noreply.github.com>
2025-07-17 11:29:04 -0300

    Prática 6 – Parte 3

--- app/src/main/java/com/example/weatherapp/HomeActivity.kt
+++ app/src/main/java/com/example/weatherapp/HomeActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.weatherapp.ui.CityDialog
@@ -35,6 +36,7 @@ import com.example.weatherapp.ui.nav.BottomNavItem
 import com.example.weatherapp.ui.nav.MainNavHost
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 import androidx.navigation.NavDestination.Companion.hasRoute
+import com.example.weatherapp.db.fb.FBDatabase
 import com.example.weatherapp.ui.nav.Route
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
@@ -47,12 +49,16 @@ class HomeActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             val navController = rememberNavController();
-            val viewModel : MainViewModel by viewModels();
             var showDialog by remember { mutableStateOf(false) };
             val currentRoute = navController.currentBackStackEntryAsState()
             val showButton = currentRoute.value?.destination?.hasRoute(Route.List::class) == true
             val launcher = rememberLauncherForActivityResult(contract =
                 ActivityResultContracts.RequestPermission(), onResult = {} )
+            val fbDB = remember { FBDatabase() }
+            val viewModel : MainViewModel = viewModel(
+                factory = MainViewModelFactory(fbDB)
+            )
+
 
             WeatherAppTheme {
                 if (showDialog) CityDialog(
